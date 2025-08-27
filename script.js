020faffa777b2d2673ddf95aa6e9a8f9264ea93a
Vishwajeet Biradar <vishwabirdar96@gmail.com>
2025-08-27 11:47:05 +0530

    git commit -m "Add initial project files"

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar style when scrolling
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
